78656670b557441f60e3256d3f54f9a0c950c632
Geoff Finch <gfinch1@gmail.com>
2018-08-11 13:53:43 -0500

    additional activity implementation and theming

--- app/src/main/java/net/raginggeek/helloandroidsafari/MainActivity.java
+++ app/src/main/java/net/raginggeek/helloandroidsafari/MainActivity.java
@@ -1,13 +1,65 @@
 package net.raginggeek.helloandroidsafari;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private TextView textView;
+    private EditText editText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "in onCreate");
+
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+
+        Button helloButton = findViewById(R.id.helloButton);
+        helloButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sayHello(view);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    public void sayHello(View view) {
+        String name = editText.getText().toString();
+        textView.setText(String.format("Hello, %s!", name));
     }
 }
